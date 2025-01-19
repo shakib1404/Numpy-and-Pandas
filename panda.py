d901ce26de7d7b887cf95fcedf4f1f82f7d954d1
shakib1404 <bsse1404@iit.du.ac.bd>
2025-01-19 20:05:56 +0600

    @

--- panda.py
+++ panda.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 
 dataset={
     'cars':["bmw","audi","nice"],
@@ -6,5 +6,16 @@ dataset={
     
 }
 
-myvar=pandas.DataFrame(dataset)
+myvar=pd.DataFrame(dataset)
 print(myvar)
+
+a=[3,5,7]
+myv2=pd.Series(a)
+print(myv2)
+print(myv2[0])
+myv3=pd.Series(a,index=["x","y","z"])
+print(myv3)
+print(myv3["y"])
+calories={"day1":420, "day2":450, "day3":550}
+myv4=pd.Series(calories,index=["day1","day2"])
+print(myv4)
